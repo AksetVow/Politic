275fe045256df131238ab780644a1ff5d88a5e8f
rslyvka@gmail.com <rslyvka@gmail.com>
2017-03-08 19:39:50 +0200

    Fix Test Project

--- Test/Properties/AssemblyInfo.cs
+++ Test/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Parser.Test")]
+[assembly: AssemblyTitle("Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Parser.Test")]
+[assembly: AssemblyProduct("Test")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2b7d4373-04f3-436b-ba24-af8bfd0152de")]
+[assembly: Guid("e1469b4d-70fa-4c91-b391-6b4a43629e33")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Test/VoteForBillTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parser;
+using System.IO;
+using System.Linq;
+
+namespace Parser.Test
+{
+    [TestClass]
+    public class VoteForBillTests
+    {
+        private string _html;
+        private VotePageParser _parser = new VotePageParser();
+
+        [TestMethod]
+        public void Setup()
+        {
+            _html = File.ReadAllText("Resources/VotePageExample.txt");
+        }
+
+        [TestMethod]
+        public void TestParsingVotePage()
+        {
+            
+        }
+
+        [TestMethod]
+        public void TestParsingDeputy()
+        {
+            var deputies = _parser.ParseDeputy(_html);
+            deputies.Count().Should().Be(450);
+        } 
+    }
+}
